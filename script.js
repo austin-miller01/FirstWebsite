e542a4d02de1538f19677810686cb753e8889fcf
austin miller <austi@MacBook-Air.local>
2021-08-05 11:54:24 -0600

    Changes to just dark and light mode

--- script.js
+++ script.js
@@ -24,17 +24,17 @@ function setTheme(mode){
 		document.getElementById('theme-style').href = 'default.css'
 	}
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'dark.css'
 	}
 
-	if(mode == 'green'){
+	/*if(mode == 'green'){
 		document.getElementById('theme-style').href = 'green.css'
 	}
 
 	if(mode == 'purple'){
 		document.getElementById('theme-style').href = 'purple.css'
-	}
+	}*/
 
 	localStorage.setItem('theme', mode)
 }
